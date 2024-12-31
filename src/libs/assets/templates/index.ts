f83fe135c20c3f0bc49e37147777c7579b783540
mokyabun <mokyabun@gmail.com>
2024-12-31 19:25:57 +0900

    fix: fix import file name

--- src/libs/assets/templates/index.ts
+++ src/libs/assets/templates/index.ts
@@ -1,3 +1,3 @@
-export { default as Font } from './Font.eta?raw'
-export { default as Main } from './Main.eta?raw'
-export { default as Variables } from './Variables.eta?raw'
+export { default as Font } from './font.eta?raw'
+export { default as Main } from './main.eta?raw'
+export { default as Variables } from './variables.eta?raw'
