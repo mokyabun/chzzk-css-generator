a2fb3a2dcde287b189a46329917e89ea92110e27
mokyabun <mo@kya.sh>
2025-03-18 14:29:29 +0900

    refactor: remove unused variables

--- src/routes/effect/effects.ts
+++ src/routes/effect/effects.ts
@@ -1,125 +1,37 @@
 export const effects = {
     fadeIn: [
-        "fadeIn",
-        "fadeInDown",
-        "fadeInDownBig",
-        "fadeInLeft",
-        "fadeInLeftBig",
-        "fadeInRight",
-        "fadeInRightBig",
-        "fadeInUp",
-        "fadeInUpBig",
-        "fadeInTopLeft",
-        "fadeInTopRight",
-        "fadeInBottomLeft",
-        "fadeInBottomRight"
+        'fadeIn',
+        'fadeInDown',
+        'fadeInDownBig',
+        'fadeInLeft',
+        'fadeInLeftBig',
+        'fadeInRight',
+        'fadeInRightBig',
+        'fadeInUp',
+        'fadeInUpBig',
+        'fadeInTopLeft',
+        'fadeInTopRight',
+        'fadeInBottomLeft',
+        'fadeInBottomRight',
     ],
-    slideIn: [
-        "slideInDown",
-        "slideInLeft",
-        "slideInRight",
-        "slideInUp"
-    ],
-    bounceIn: [
-        "bounceIn",
-        "bounceInDown",
-        "bounceInLeft",
-        "bounceInRight",
-        "bounceInUp"
-    ],
-    flipIn: [
-        "flipInX",
-        "flipInY"
-    ],
-    lightSpeedIn: [
-        "lightSpeedIn",
-        "lightSpeedInRight",
-        "lightSpeedInLeft"
-    ],
-    rotateIn: [
-        "rotateIn",
-        "rotateInDownLeft",
-        "rotateInDownRight",
-        "rotateInUpLeft",
-        "rotateInUpRight"
-    ],
-    zoomIn: [
-        "zoomIn",
-        "zoomInDown",
-        "zoomInLeft",
-        "zoomInRight",
-        "zoomInUp"
-    ],
+    slideIn: ['slideInDown', 'slideInLeft', 'slideInRight', 'slideInUp'],
+    bounceIn: ['bounceIn', 'bounceInDown', 'bounceInLeft', 'bounceInRight', 'bounceInUp'],
+    flipIn: ['flipInX', 'flipInY'],
+    lightSpeedIn: ['lightSpeedIn', 'lightSpeedInRight', 'lightSpeedInLeft'],
+    rotateIn: ['rotateIn', 'rotateInDownLeft', 'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight'],
+    zoomIn: ['zoomIn', 'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp'],
     attention: [
-        "bounce",
-        "flash",
-        "pulse",
-        "rubberBand",
-        "shakeX",
-        "shakeY",
-        "headShake",
-        "swing",
-        "tada",
-        "wobble",
-        "jello",
-        "heartBeat"
+        'bounce',
+        'flash',
+        'pulse',
+        'rubberBand',
+        'shakeX',
+        'shakeY',
+        'headShake',
+        'swing',
+        'tada',
+        'wobble',
+        'jello',
+        'heartBeat',
     ],
 }
-
-export const bounceIn = [
-    "bounceIn",
-    "bounceInDown",
-    "bounceInLeft",
-    "bounceInRight",
-    "bounceInUp"
-]
-
-export const fadeIn = [
-    "fadeIn",
-    "fadeInDown",
-    "fadeInDownBig",
-    "fadeInLeft",
-    "fadeInLeftBig",
-    "fadeInRight",
-    "fadeInRightBig",
-    "fadeInUp",
-    "fadeInUpBig",
-    "fadeInTopLeft",
-    "fadeInTopRight",
-    "fadeInBottomLeft",
-    "fadeInBottomRight"
-]
-
-export const flipIn = [
-    "flipInX",
-    "flipInY"
-]
-
-export const lightSpeedIn = [
-    "lightSpeedIn",
-    "lightSpeedInRight",
-    "lightSpeedInLeft"
-]
-
-export const rotateIn = [
-    "rotateIn",
-    "rotateInDownLeft",
-    "rotateInDownRight",
-    "rotateInUpLeft",
-    "rotateInUpRight"
-]
-
-export const slideIn = [
-    "slideInDown",
-    "slideInLeft",
-    "slideInRight",
-    "slideInUp"
-]
-
-export const zoomIn = [
-    "zoomIn",
-    "zoomInDown",
-    "zoomInLeft",
-    "zoomInRight",
-    "zoomInUp"
-]
